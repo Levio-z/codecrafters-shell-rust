7791bb70139f586cad6445c8cc902a2b9fe6da2b
Levio-Z <67247011+Levio-z@users.noreply.github.com>
2025-12-20 20:50:30 +0800

    Refactor and Implement 1. The pwd builtin #ei0 2. The cd builtin: Absolute paths #ra6 3.The cd builtin: Relative paths #gq9 4. The cd builtin: Home directory #gp4

--- src/main.rs
+++ src/main.rs
@@ -3,68 +3,100 @@ use std::io::{self, Write};
 use std::{
     path::{Path, PathBuf},
     process::Command,
+    sync::LazyLock,
 };
 
+use anyhow::Context;
 use is_executable::IsExecutable;
-fn main() {
-    // TODO: Uncomment the code below to pass the first stage
-    // TODO: unwrap 使用context优化
 
-    loop {
+static GLOBAL_VEC: LazyLock<Vec<PathBuf>> = LazyLock::new(|| {
+    let path = std::env::var("PATH").unwrap_or("".to_string());
+    std::env::split_paths(&std::ffi::OsStr::new(&path)).collect::<Vec<_>>()
+});
+
+static HOME_DIR: LazyLock<String> =
+    LazyLock::new(|| std::env::var("HOME").unwrap_or("".to_string()));
+
+fn main() -> anyhow::Result<()> {
+    'outer: loop {
         print!("$ ");
         io::stdout().flush().unwrap();
-        let mut command = String::new();
-        io::stdin().read_line(&mut command).unwrap();
-        let command_trim = command.trim();
-        let mut command_list = command.split_whitespace();
-        let bin_command = command_list.next().unwrap().trim();
+        let mut line = String::new();
+        io::stdin()
+            .read_line(&mut line)
+            .context("read line failed")?;
 
-        let path = std::env::var("PATH").unwrap_or("".to_string());
-        let paths: Vec<PathBuf> =
-            std::env::split_paths(&std::ffi::OsStr::new(&path)).collect::<Vec<_>>();
-        
-        if command_trim == "exit" {
-            break;
-        } else if command_trim.starts_with("echo") {
-            println!("{}", command_trim.strip_prefix("echo ").unwrap());
-        } else if command_trim.starts_with("type") {
-            let command_type = command_trim.strip_prefix("type ").unwrap();
-            if command_type == "exit" || command_type == "echo" || command_type == "type" {
-                println!("{} is a shell builtin", command_type);
-            } else if let Some(file_path) = find_executable_file_in_paths(command_type, paths) {
-                let file_path = file_path.canonicalize().unwrap();
-                println!("{} is {}", command_type, file_path.display());
-            } else {
-                println!("{command_type}: not found");
+        let line_trim = line.trim();
+
+        let mut params = line_trim.split_whitespace();
+        let command = params.next().context("command is empty")?.trim();
+
+        match command {
+            "exit" => break 'outer,
+            "echo" => println!(
+                "{}",
+                line_trim
+                    .strip_prefix("echo ")
+                    .context("echo command is empty")?
+            ),
+            "type" => {
+                let command_type = params.next().context("type command is empty")?.trim();
+                // TODO:使用 enum 优化
+                match command_type {
+                    "exit" | "echo" | "type" | "pwd" | "cd" => {
+                        println!("{} is a shell builtin", command_type)
+                    }
+                    _ => match find_executable_file_in_paths(command_type, &GLOBAL_VEC) {
+                        Some(file_path) => println!("{} is {}", command_type, file_path.display()),
+                        None => println!("{command_type}: not found"),
+                    },
+                }
             }
-        } else if let Some(file_path) = find_executable_file_in_paths(bin_command, paths) {
-            
-            let _ = Command::new(file_path.file_name().unwrap())
-                .args(command_list)
-                .status()
-                .expect("failed to execute");
-        } else {
-            println!("{}: command not found", command_trim);
+            "pwd" => println!(
+                "{}",
+                std::env::current_dir().context("pwd failed")?.display()
+            ),
+
+            "cd" => {
+                let dir = params.next().context("cd command is empty")?.trim();
+                if params.next().is_some() {
+                    println!("bash: cd: too many arguments");
+                    break 'outer;
+                }
+                let dir = if dir == "~" { &HOME_DIR } else { dir };
+                match std::env::set_current_dir(dir).context("cd failed") {
+                    Ok(_) => {}
+                    Err(_) => println!("cd: {}: No such file or directory", dir),
+                }
+            }
+            _ => match find_executable_file_in_paths(command, &GLOBAL_VEC) {
+                Some(file_path) => {
+                    let _ = Command::new(file_path.file_name().context("file name is empty")?)
+                        .args(params)
+                        .status()
+                        .context("failed to execute")?;
+                }
+                None => println!("{}: command not found", command),
+            },
         }
     }
+    Ok(())
 }
 
 fn find_executable_file_in_path(executable_file: &str, path: &Path) -> Option<PathBuf> {
     let file_path = path.join(executable_file);
-
     if file_path.is_file() && file_path.is_executable() {
         return Some(file_path);
     }
-
     None
 }
 
-fn find_executable_file_in_paths(executable_file: &str, paths: Vec<PathBuf>) -> Option<PathBuf> {
+fn find_executable_file_in_paths(executable_file: &str, paths: &Vec<PathBuf>) -> Option<PathBuf> {
     for path in paths {
-        if path.exists() || path.is_dir() {
-            if let Some(file_path) = find_executable_file_in_path(executable_file, &path) {
-                return Some(file_path);
-            }
+        if (path.exists() || path.is_dir())
+            && let Some(file_path) = find_executable_file_in_path(executable_file, path)
+        {
+            return Some(file_path);
         }
     }
     None
