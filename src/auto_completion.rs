763b59696c0c9b0f267bd8470c810a7842bc07f9
Levio-Z <67247011+Levio-z@users.noreply.github.com>
2025-12-26 19:39:22 +0800

    Implement Autocompletion 1. Builtin completion #qp2 2. Completion with arguments #gm9 3. Missing completions #qm8 4. Executable completion #gy5 5. Multiple completions #wh6 6. Partial completions #wt6

---
+++ src/auto_completion.rs
@@ -0,0 +1,86 @@
+use std::{borrow::Cow, sync::LazyLock};
+
+use radix_trie::{Trie, TrieCommon};
+use rustyline::{
+    Changeset, Context, Helper,
+    completion::{Completer, Pair},
+    error::ReadlineError,
+    highlight::Highlighter,
+    hint::Hinter,
+    line_buffer::LineBuffer,
+    validate::{ValidationContext, ValidationResult, Validator},
+};
+
+use crate::{BuildinCommand, GLOBAL_VEC, find_all_executable_file_in_paths};
+pub(crate) struct MyCompleter;
+use strum::IntoEnumIterator;
+
+static GLOBAL_TRIES: LazyLock<Trie<String, ()>> = LazyLock::new(|| {
+    let iter = BuildinCommand::iter();
+    let mut commands: Vec<String> = iter.map(|cmd| cmd.to_string()).collect();
+    commands.extend(
+        find_all_executable_file_in_paths(&GLOBAL_VEC)
+            .iter()
+            .filter_map(|path| {
+                path.file_name()                // Option<&OsStr>
+                .and_then(|name| name.to_str()) // Option<&str>
+                .map(|s| s.to_string())
+            }),
+    );
+    commands
+        .into_iter()
+        .map(|cmd| (cmd.to_string(), ()))
+        .collect::<Trie<String, ()>>()
+});
+
+impl Completer for MyCompleter {
+    type Candidate = Pair;
+
+    fn complete(
+        &self,
+        line: &str,
+        pos: usize,
+        _ctx: &Context<'_>,
+    ) -> Result<(usize, Vec<Pair>), ReadlineError> {
+        let _start = 0; // 从行首开始补全
+        let prefix = &line[..pos];
+        let prefix_keys: Vec<Pair> = GLOBAL_TRIES
+            .get_raw_descendant(prefix)
+            .map(|trie|
+            trie.keys()
+            .map(|k| Pair {
+                display: k.clone(),
+                replacement: k.clone(),
+            })
+            .collect())
+            .unwrap_or_else(|| Vec::new()) ;
+        Ok((0, prefix_keys))
+    }
+    fn update(&self, line: &mut LineBuffer, start: usize, elected: &str, cl: &mut Changeset) {
+        let end = line.pos();
+        let elected = if let Some(sub_trie) = GLOBAL_TRIES.subtrie(elected)
+            && sub_trie.is_leaf()
+        {
+            Cow::Owned(elected.to_string() + " ")
+        } else {
+            Cow::Borrowed(elected)
+        };
+        line.replace(start..end, &elected, cl);
+    }
+}
+
+impl Helper for MyCompleter {} // 必须实现 Helper trait
+impl Hinter for MyCompleter {
+    type Hint = String;
+    fn hint(&self, _line: &str, _pos: usize, _ctx: &rustyline::Context<'_>) -> Option<String> {
+        None // 不提供提示
+    }
+}
+
+impl Highlighter for MyCompleter {} // 空实现
+
+impl Validator for MyCompleter {
+    fn validate(&self, _ctx: &mut ValidationContext) -> Result<ValidationResult, ReadlineError> {
+        Ok(ValidationResult::Valid(None)) // 始终认为输入合法
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -7,16 +7,17 @@ use std::{
     sync::LazyLock,
     vec,
 };
-
+mod auto_completion;
 use anyhow::Context;
+use auto_completion::MyCompleter;
 use is_executable::IsExecutable;
-
-static GLOBAL_VEC: LazyLock<Vec<PathBuf>> = LazyLock::new(|| {
+use rustyline::{Editor, config::Configurer, error::ReadlineError,config::{Config, CompletionType}};
+use strum::{AsRefStr, Display, EnumIter, EnumString};
+pub static GLOBAL_VEC: LazyLock<Vec<PathBuf>> = LazyLock::new(|| {
     let path = std::env::var("PATH").unwrap_or("".to_string());
     std::env::split_paths(&std::ffi::OsStr::new(&path)).collect::<Vec<_>>()
 });
-
-static HOME_DIR: LazyLock<String> =
+pub static HOME_DIR: LazyLock<String> =
     LazyLock::new(|| std::env::var("HOME").unwrap_or("".to_string()));
 /// 表示一个命令行参数
 #[derive(Debug, Clone)]
@@ -41,72 +42,96 @@ impl Token {
         self.content.push(content);
     }
     fn set_redirect(&mut self, redirect: String, append: bool) -> anyhow::Result<()> {
-        let file = OpenOptions::new()
-            .append(append)
-            .create(true)
-            .write(true)
-            .open(&redirect);
+        create_or_truncate_file(&redirect, append)?;
         self.redirect = Some(redirect);
         Ok(())
     }
     fn set_redirect_err(&mut self, redirect_err: String, append: bool) -> anyhow::Result<()> {
-        OpenOptions::new()
-            .append(append)
-            .create(true)
-            .write(true)
-            .open(&redirect_err)?;
+        create_or_truncate_file(&redirect_err, append)?;
         self.redirect_err = Some(redirect_err);
         Ok(())
     }
 }
 
+fn create_or_truncate_file(path: &str, append: bool) -> anyhow::Result<()> {
+    OpenOptions::new()
+        .truncate(!append)
+        .create(true)
+        .write(true)
+        .open(path)?;
+    Ok(())
+}
+
 fn main() -> anyhow::Result<()> {
+    let config = Config::builder()
+    .completion_type(CompletionType::List) // 多候选时列出
+    .bell_style(rustyline::config::BellStyle::Audible)               // 歧义时响铃
+    .build();
+
+    let completer = MyCompleter;
+    let mut rl = Editor::with_config(config)?;
+    rl.set_completion_type(rustyline::CompletionType::List);
+    rl.set_helper(Some(completer));
     loop {
-        print!("$ ");
-        io::stdout().flush().unwrap();
-        let mut line = String::new();
-        io::stdin()
-            .read_line(&mut line)
-            .context("read line failed")?;
-
-        let line_trim = line.trim();
-        let tokens = split_quotes(line_trim);
-        let iter = tokens.into_iter();
-        for token in iter {
-            let last_result = handle(token.content.into_iter());
-
-            match token.redirect_err {
-                None => {
-                    if !last_result.stderr.is_empty() {
-                        eprint!("{}", last_result.stderr);
-                    }
-                }
-                Some(redirect_err) => {
-                    let mut f = OpenOptions::new()
-                        .append(token.append_err)
-                        .write(true)
-                        .open(redirect_err)?;
-                    std::io::Write::write_all(&mut f, last_result.stderr.as_bytes())
-                        .context("write file failed")?;
-                }
+        match rl.readline("$ ") {
+            Ok(line) => {
+                parse_and_handle_line(&line)?;
             }
-            match token.redirect {
-                None => {
-                    if !last_result.stdout.is_empty() {
-                        print!("{}", last_result.stdout);
-                    }
-                }
-                Some(redirect) => {
-                    let mut f = OpenOptions::new()
-                        .append(token.append_out)
-                        .write(true)
-                        .open(redirect)?;
-                    std::io::Write::write_all(&mut f, last_result.stdout.as_bytes())
-                        .context("write file failed")?;
-                }
+            Err(ReadlineError::Interrupted) => {
+                println!("^C");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
             }
         }
     }
+    Ok(())
+}
+
+fn parse_and_handle_line(line: &str) -> anyhow::Result<()> {
+    let line_trim = line.trim();
+    let tokens = split_quotes(line_trim);
+    let iter = tokens.into_iter();
+    for token in iter {
+        let last_result = handle(token.content.into_iter());
+
+        match token.redirect_err {
+            None => {
+                if !last_result.stderr.is_empty() {
+                    eprint!("{}", last_result.stderr);
+                }
+            }
+            Some(redirect_err) => {
+                let mut f = OpenOptions::new()
+                    .append(token.append_err)
+                    .write(true)
+                    .open(redirect_err)?;
+                std::io::Write::write_all(&mut f, last_result.stderr.as_bytes())
+                    .context("write file failed")?;
+            }
+        }
+        match token.redirect {
+            None => {
+                if !last_result.stdout.is_empty() {
+                    print!("{}", last_result.stdout);
+                }
+            }
+            Some(redirect) => {
+                let mut f = OpenOptions::new()
+                    .append(token.append_out)
+                    .write(true)
+                    .open(redirect)?;
+                std::io::Write::write_all(&mut f, last_result.stdout.as_bytes())
+                    .context("write file failed")?;
+            }
+        }
+    }
+    Ok(())
 }
 /// 表示一个命令执行结果
 #[derive(Debug, Clone)]
@@ -117,100 +142,101 @@ struct CommandResult {
     #[allow(dead_code)]
     exit_code: i32, // 退出码，0表示成功
 }
+impl Default for CommandResult {
+    fn default() -> Self {
+        Self {
+            stdout: "".to_string(),
+            stderr: "".to_string(),
+            exit_code: 0,
+        }
+    }
+}
+impl CommandResult {
+    fn new_with_stdout(stdout: String) -> Self {
+        Self {
+            stdout,
+            ..Default::default()
+        }
+    }
+    fn new_with_stderr(stderr: String) -> Self {
+        Self {
+            stderr,
+            exit_code: 1,
+            ..Default::default()
+        }
+    }
+}
+#[derive(Debug, Clone, Copy, PartialEq, Display, EnumString, AsRefStr, EnumIter)]
+#[strum(serialize_all = "lowercase")]
+pub enum BuildinCommand {
+    Exit,
+    Pwd,
+    Cd,
+    Echo,
+    Type,
+}
 
 fn handle(mut params: impl Iterator<Item = String>) -> CommandResult {
     let command = params.next().context("command is empty");
     let command = match command {
         Ok(command) => command,
         Err(e) => {
-            return CommandResult {
-                stdout: "".to_string(),
-                stderr: e.to_string(),
-                exit_code: 1,
-            };
+            return CommandResult::new_with_stderr(e.to_string());
         }
     };
 
-    match command.as_str() {
-        "exit" => std::process::exit(0),
-        "echo" => CommandResult {
-            stdout: format!("{}\n", params.collect::<Vec<_>>().join(" ")),
-            stderr: "".to_string(),
-            exit_code: 0,
-        },
-        "type" => {
+    match command.parse::<BuildinCommand>() {
+        Ok(BuildinCommand::Exit) => std::process::exit(0),
+        Ok(BuildinCommand::Echo) => {
+            CommandResult::new_with_stdout(format!("{}\n", params.collect::<Vec<_>>().join(" ")))
+        }
+        Ok(BuildinCommand::Type) => {
             let command_type = params.next().context("type command is empty");
             let command_type = match command_type {
                 Ok(command_type) => command_type,
-                Err(e) => {
-                    return CommandResult {
-                        stdout: "".to_string(),
-                        stderr: e.to_string(),
-                        exit_code: 1,
-                    };
-                }
+                Err(e) => return CommandResult::new_with_stderr(e.to_string()),
             };
             // TODO:使用 enum 优化
-            match command_type.as_str() {
-                "exit" | "echo" | "type" | "pwd" | "cd" => CommandResult {
-                    stdout: format!("{} is a shell builtin\n", command_type),
-                    stderr: "".to_string(),
-                    exit_code: 0,
-                },
+            match command_type.parse::<BuildinCommand>() {
+                Ok(_) => {
+                    CommandResult::new_with_stdout(format!("{} is a shell builtin\n", command_type))
+                }
                 _ => match find_executable_file_in_paths(&command_type, &GLOBAL_VEC) {
-                    Some(file_path) => CommandResult {
-                        stdout: format!("{} is {}\n", command_type, file_path.display()),
-                        stderr: "".to_string(),
-                        exit_code: 0,
-                    },
-                    None => CommandResult {
-                        stdout: "".to_string(),
-                        stderr: format!("{command_type}: not found\n"),
-                        exit_code: 1,
-                    },
+                    Some(file_path) => CommandResult::new_with_stdout(format!(
+                        "{} is {}\n",
+                        command_type,
+                        file_path.display()
+                    )),
+                    None => CommandResult::new_with_stderr(format!("{command_type}: not found\n")),
                 },
             }
         }
-        "pwd" => CommandResult {
-            stdout: std::env::current_dir()
+        Ok(BuildinCommand::Pwd) => CommandResult::new_with_stdout(
+            std::env::current_dir()
                 .context("pwd failed\n")
                 .map(|dir| format!("{}\n", dir.display()))
                 .unwrap_or("".to_string()),
-            stderr: "".to_string(),
-            exit_code: 0,
-        },
-        "cd" => {
+        ),
+        Ok(BuildinCommand::Cd) => {
             let dir = params.next().context("cd command is empty\n");
             let dir = match dir {
                 Ok(dir) => dir,
                 Err(_) => {
-                    return CommandResult {
-                        stdout: "".to_string(),
-                        stderr: "cd: missing operand\n".to_string(),
-                        exit_code: 1,
-                    };
+                    return CommandResult::new_with_stderr("cd: missing operand\n".to_string());
                 }
             };
 
             if params.next().is_some() {
-                CommandResult {
-                    stdout: "".to_string(),
-                    stderr: "bash: cd: too many arguments\n".to_string(),
-                    exit_code: 1,
-                }
+                CommandResult::new_with_stderr("bash: cd: too many arguments\n".to_string())
             } else {
                 let dir = if dir == "~" { &HOME_DIR } else { &dir };
                 match std::env::set_current_dir(dir).context("cd failed\n") {
-                    Ok(_) => CommandResult {
-                        stdout: "".to_string(),
-                        stderr: "".to_string(),
-                        exit_code: 0,
-                    },
-                    Err(_) => CommandResult {
-                        stdout: "".to_string(),
-                        stderr: format!("cd: {}: No such file or directory\n", dir),
-                        exit_code: 1,
-                    },
+                    Ok(_) => CommandResult::default(),
+
+                    Err(_) => CommandResult::new_with_stderr(format!(
+                        "cd: {}: No such file or directory\n",
+                        dir
+                    )),
                 }
             }
         }
@@ -218,11 +244,10 @@ fn handle(mut params: impl Iterator<Item = String>) -> CommandResult {
             Some(file_path) => {
                 let file_name = file_path.file_name().context("file name is empty");
                 if file_name.is_err() {
-                    return CommandResult {
-                        stdout: "".to_string(),
-                        stderr: format!("{}: file name is empty\n", command),
-                        exit_code: 1,
-                    };
+                    return CommandResult::new_with_stderr(format!(
+                        "{}: file name is empty\n",
+                        command
+                    ));
                 }
                 Command::new(file_name.as_ref().unwrap())
                     .args(params)
@@ -232,25 +257,18 @@ fn handle(mut params: impl Iterator<Item = String>) -> CommandResult {
                         stderr: String::from_utf8_lossy(&output.stderr).to_string(),
                         exit_code: output.status.code().unwrap_or(1),
                     })
-                    .unwrap_or_else(|_| CommandResult {
-                        stdout: "".to_string(),
-                        stderr: format!("{}: failed to execute\n", command),
-                        exit_code: 1,
+                    .unwrap_or_else(|_| {
+                        CommandResult::new_with_stderr(format!("{}: failed to execute\n", command))
                     })
             }
-            None => CommandResult {
-                stdout: "".to_string(),
-                stderr: format!("{}: command not found\n", command),
-                exit_code: 1,
-            },
+            None => CommandResult::new_with_stderr(format!("{}: command not found\n", command)),
         },
     }
 }
 
-fn find_executable_file_in_path(executable_file: &str, path: &Path) -> Option<PathBuf> {
-    let file_path = path.join(executable_file);
-    if file_path.is_file() && file_path.is_executable() {
-        return Some(file_path);
+fn find_executable_file_in_path(path: &Path) -> Option<PathBuf> {
+    if path.is_file() && path.is_executable() {
+        return Some(path.to_path_buf());
     }
     None
 }
@@ -258,13 +276,33 @@ fn find_executable_file_in_path(executable_file: &str, path: &Path) -> Option<Pa
 fn find_executable_file_in_paths(executable_file: &str, paths: &Vec<PathBuf>) -> Option<PathBuf> {
     for path in paths {
         if (path.exists() || path.is_dir())
-            && let Some(file_path) = find_executable_file_in_path(executable_file, path)
+            && let Some(file_path) = find_executable_file_in_path(&path.join(executable_file))
         {
             return Some(file_path);
         }
     }
     None
 }
+
+use std::fs;
+
+fn find_all_executable_file_in_paths(paths: &[PathBuf]) -> Vec<PathBuf> {
+    paths
+        .iter()
+        .filter(|path| path.exists() && path.is_dir())
+        .flat_map(|dir| {
+            fs::read_dir(dir)
+                .map(|rd| {
+                    Box::new(
+                        rd.filter_map(|entry| entry.ok())
+                            .filter_map(|entry| find_executable_file_in_path(&entry.path())),
+                    ) as Box<dyn Iterator<Item = PathBuf>>
+                })
+                .unwrap_or_else(|_| Box::new(std::iter::empty()))
+        })
+        .collect()
+}
+
 enum MatchType {
     Default,
     DoubleQuote,
@@ -301,15 +339,15 @@ fn split_quotes(line: &str) -> Vec<Token> {
                 '\'' => match_type = MatchType::SingleQuote,
                 '"' => match_type = MatchType::DoubleQuote,
                 '\\' => match_type = MatchType::Escaping,
-                '>' => {              
-                        if redirect {
-                            token.append_out = true;
-                            continue;
-                        } else if redirect_err {
-                            token.append_err = true;
-                            continue;
-                        }
-                        
+                '>' => {
+                    if redirect {
+                        token.append_out = true;
+                        continue;
+                    } else if redirect_err {
+                        token.append_err = true;
+                        continue;
+                    }
+
                     if !string.is_empty() {
                         if string.ends_with("1") {
                             string.pop();
